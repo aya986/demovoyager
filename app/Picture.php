d456aaa41c55c2f6194a6e9cb6d7f5e100cc42ed
aya <ayoob.khodadadi@gmail.com>
2021-02-25 01:06:24 +0330

    add picture table for posts

---
+++ app/Picture.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class BannerCategory.
+ */
+class Picture extends Model
+{
+    public $table = "pictures";
+    public $timestamps = false;
+}
